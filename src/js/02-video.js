fc05aa770c95af63eacd464570ecff380aa435dc
svitlanapl <sveta_pr@i.ua>
2022-10-02 00:11:41 +0300

    Tasks 1,2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,21 @@
+import Player from '@vimeo/player';
+// console.log(Player);
+
+// const throttle = require('lodash.throttle');
+import throttle from 'lodash.throttle';
+
+const iframeEl = document.querySelector('iframe');
+const player = new Player(iframeEl);
+
+const KEY_LOCALSTORAGE = "videoplayer-current-time";
+
+const currentTime = localStorage.getItem(KEY_LOCALSTORAGE);
+if (currentTime) {
+    player.setCurrentTime(currentTime);
+}
+
+player.on('timeupdate', throttle(function (data) {
+    localStorage.setItem(KEY_LOCALSTORAGE, data.seconds);
+}, 1000)
+);
 
